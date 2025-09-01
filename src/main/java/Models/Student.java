8d59e201387c68796b9bdc22c30d7deecdb719bb
narayan.rguttal@thermofisher.com <narayan.rguttal@thermofisher.com>
2025-09-01 19:00:44 +0530

    Added ORM related changes

--- src/main/java/Models/Student.java
+++ src/main/java/Models/Student.java
@@ -1,8 +1,17 @@
 package Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students")
 public class Student 
 {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private int age;
