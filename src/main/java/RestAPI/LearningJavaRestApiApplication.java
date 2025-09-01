e8855b8235263ca48fc790673036680c438af7cf
narayan.rguttal@thermofisher.com <narayan.rguttal@thermofisher.com>
2025-09-01 19:12:44 +0530

    Moving Main class to right project structure

--- src/main/java/RestAPI/LearningJavaRestApiApplication.java
+++ src/main/java/RestAPI/LearningJavaRestApiApplication.java
@@ -2,9 +2,10 @@ package RestAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
+@ComponentScan(basePackages = {"RestAPI", "Services", "Models", "Repository", "Controllers"})
 public class LearningJavaRestApiApplication {
 
 	public static void main(String[] args) {
